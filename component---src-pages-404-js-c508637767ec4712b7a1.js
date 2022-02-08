f9c93e098cd7edef69a0ebfe7ac9d36e44db58b7
patrykzych1 <patzycc@gmail.com>
2022-02-08 22:24:47 +0100

    Updates

---
+++ component---src-pages-404-js-c508637767ec4712b7a1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),u=t.n(a);e.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c508637767ec4712b7a1.js.map
